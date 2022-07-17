5534ea1df37d37be627cd122a62ae0e3dc939f0a
syafiqmk <syafiqmuh.k@gmail.com>
2022-07-17 10:25:08 +0800

    update function keyup search

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ $("document").ready(function() {
     });
 
     $("#search-input").on("keyup", function(e) {
-        if(e.keyCode === 13) {
+        if(e.which === 13) {
             e.preventDefault();
             $("#search").html('');
             $("#movie-row").html('');
